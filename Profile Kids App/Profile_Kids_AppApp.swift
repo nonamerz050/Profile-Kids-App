5736b7420b0fd66469ca6c4f40c4b0e79b5b0ea5
Nonamerz <a.g.kolesnik@yandex.ru>
2021-11-10 16:40:49 +0300

    Initial Commit

---
+++ Profile Kids App/Profile_Kids_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_Kids_AppApp.swift
+//  Profile Kids App
+//
+//  Created by MacBook Pro on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Profile_Kids_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
