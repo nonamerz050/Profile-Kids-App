9ad83fb76146fe06b06fe1d6bd932dc0627da1af
Nonamerz <a.g.kolesnik@yandex.ru>
2021-11-16 13:52:08 +0300

    News App need to add package SDWebImage

--- Profile Kids App/Styles.swift
+++ Profile Kids App/Styles.swift
@@ -7,24 +7,17 @@
 
 import SwiftUI
 
-struct TitleTextField: ViewModifier {
-    var color: Color = .gray
+struct TextLabel: ViewModifier {
+    var color: Color = .black
     var weight: Font.Weight = .regular
     var size: CGFloat = 17
+    var paddingTop = MagicNumber.x2
     
     func body(content: Content) -> some View {
         content
             .font(.system(size: size, weight: weight))
             .foregroundColor(color)
             .padding(.horizontal, MagicNumber.x2)
-            .padding(.top, MagicNumber.x)
-    }
-}
-
-struct BasicTextField: ViewModifier {    
-    func body(content: Content) -> some View {
-        content
-            .padding(.horizontal, MagicNumber.x2)
-            .padding(.vertical, MagicNumber.x)
+            .padding(.top, paddingTop)
     }
 }
